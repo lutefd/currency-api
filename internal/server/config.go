1b2a85f1c776aa907b9c06ce11efc00e2f8f2c3a
Luis Dourado <lfaddourado@gmail.com>
2024-08-05 10:36:27 -0300

    refactor(config): improve configuration loading with error handling
    This commit enhances the configuration loading by adding error handling
    and validation for required environment variables. Changes include:
    
    - Adding the APIKey field to the Config struct.
    - Modifying LoadConfig to return an error if required environment
    variables are not set.
     - Adding validation for REDIS_PASSWORD, REDIS_ADDR, POSTGRES_CONN,
    API_KEY, and SERVER_PORT environment variables.
    - Returning detailed error messages if any configuration values are
    missing or invalid.

--- internal/server/config.go
+++ internal/server/config.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,22 +11,51 @@ type Config struct {
 	RedisAddr    string
 	RedisPass    string
 	ServerPort   uint16
+	APIKey       string
 }
 
-func LoadConfig() Config {
-	redisPassword := os.Getenv("REDIS_PASSWORD")
-	redisAddr := os.Getenv("REDIS_ADDR")
-	serverPort := os.Getenv("SERVER_PORT")
-	postgresConn := os.Getenv("POSTGRES_CONN")
-	parsedServerPort, err := strconv.ParseUint(serverPort, 10, 16)
-	if err != nil {
-		panic(err)
+func LoadConfig() (Config, error) {
+	var config Config
+	var errors []string
+
+	config.RedisPass = os.Getenv("REDIS_PASSWORD")
+	if config.RedisPass == "" {
+		errors = append(errors, "REDIS_PASSWORD is not set")
 	}
 
-	return Config{
-		PostgresConn: postgresConn,
-		RedisAddr:    redisAddr,
-		RedisPass:    redisPassword,
-		ServerPort:   uint16(parsedServerPort),
+	config.RedisAddr = os.Getenv("REDIS_ADDR")
+	if config.RedisAddr == "" {
+		errors = append(errors, "REDIS_ADDR is not set")
 	}
+
+	config.PostgresConn = os.Getenv("POSTGRES_CONN")
+	if config.PostgresConn == "" {
+		errors = append(errors, "POSTGRES_CONN is not set")
+	}
+
+	config.APIKey = os.Getenv("API_KEY")
+	if config.APIKey == "" {
+		errors = append(errors, "API_KEY is not set")
+	}
+
+	serverPort := os.Getenv("SERVER_PORT")
+	if serverPort == "" {
+		errors = append(errors, "SERVER_PORT is not set")
+	} else {
+		parsedServerPort, err := strconv.ParseUint(serverPort, 10, 16)
+		if err != nil {
+			errors = append(errors, fmt.Sprintf("Invalid SERVER_PORT: %s", err))
+		} else {
+			config.ServerPort = uint16(parsedServerPort)
+		}
+	}
+
+	if len(errors) > 0 {
+		for _, err := range errors {
+			fmt.Println("Configuration Error:", err)
+		}
+		return Config{}, fmt.Errorf("configuration errors occurred")
+	}
+
+	return config, nil
 }
