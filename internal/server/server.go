0de3370a2982ed480e8ca33cbca24ef7c4960ad6
Luis Dourado <lfaddourado@gmail.com>
2024-08-05 11:44:50 -0300

    refactor(server): enhance server setup and graceful shutdown
    This commit refactors the server setup. Changes include:
    - Initializing repository, cache, and external API client within the
    NewServer function.
    - Adding rate updater initialization and starting it in a separate
    goroutine.
    - Enhancing the Start function to log server start and handle HTTP
    server errors.
    - Implementing a Shutdown function to gracefully shut down the server,
    close database and cache connections, and log shutdown progress.
    - Utilizing the logger package for logging server events and errors.

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,55 +4,90 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"os"
 	"time"
+
+	"github.com/Lutefd/challenge-bravo/internal/cache"
+	"github.com/Lutefd/challenge-bravo/internal/logger"
+	"github.com/Lutefd/challenge-bravo/internal/repository"
+	"github.com/Lutefd/challenge-bravo/internal/service"
+	"github.com/Lutefd/challenge-bravo/internal/worker"
 )
 
 type Server struct {
-	port   int
-	router http.Handler
-	config Config
+	config        Config
+	httpServer    *http.Server
+	router        http.Handler
+	rateUpdater   *worker.RateUpdater
+	currencyRepo  repository.CurrencyRepository
+	currencyCache cache.Cache
+	externalAPI   worker.ExternalAPIClient
 }
 
-func NewServer(config Config) *Server {
-	strPort := os.Getenv("SERVER_PORT")
-	if strPort == "" {
-		fmt.Println("port environment variable was not setted")
+func NewServer(config Config) (*Server, error) {
+	repo, err := repository.NewPostgresCurrencyRepository(config.PostgresConn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize repository: %w", err)
 	}
+	redisCache, err := cache.NewRedisCache(config.RedisAddr, config.RedisPass)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize cache: %w", err)
+	}
+	externalAPI := worker.NewOpenExchangeRatesClient(config.APIKey)
+	currencyService := service.NewCurrencyService(repo, redisCache, externalAPI)
+	rateUpdater := worker.NewRateUpdater(repo, redisCache, externalAPI, 1*time.Hour)
+
 	server := &Server{
-		port:   int(config.ServerPort),
-		config: config,
+		config:        config,
+		currencyRepo:  repo,
+		currencyCache: redisCache,
+		externalAPI:   externalAPI,
+		rateUpdater:   rateUpdater,
 	}
-	server.registerRoutes()
-	return server
-}
 
-func (s *Server) Start(ctx context.Context) error {
-	fmt.Println("Starting server on port 8080")
-	ch := make(chan error, 1)
-	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", s.port),
-		Handler:      s.router,
+	server.registerRoutes(currencyService)
+
+	server.httpServer = &http.Server{
+		Addr:         fmt.Sprintf(":%d", config.ServerPort),
+		Handler:      server.router,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	var err error
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			ch <- fmt.Errorf("failed to start server: %w", err)
-		}
-		close(ch)
-	}()
-	select {
-	case err = <-ch:
-		return err
-	case <-ctx.Done():
-		_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
+	return server, nil
+}
 
-		return server.Shutdown(ctx)
+func (s *Server) Start(ctx context.Context) error {
+	go s.rateUpdater.Start(ctx)
+	go func() {
+		logger.Infof("Server started on port %d", s.config.ServerPort)
+		if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Errorf("HTTP server error: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	return s.Shutdown()
+}
+
+func (s *Server) Shutdown() error {
+	logger.Info("Server is shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		logger.Errorf("HTTP server shutdown error: %v", err)
+		return err
 	}
+	if err := s.currencyRepo.Close(); err != nil {
+		logger.Errorf("Database connection close error: %v", err)
+		return err
+	}
+
+	if err := s.currencyCache.Close(); err != nil {
+		logger.Errorf("Cache connection close error: %v", err)
+		return err
+	}
+
+	logger.Info("Server shutdown complete")
+	return nil
 }
