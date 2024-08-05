ad81b096eca0c839604095b147fa77f22ff1ae55
Luis Dourado <lfaddourado@gmail.com>
2024-08-05 13:05:28 -0300

    feat(routes): add authentication, role-based access, and rate limiting to routes
    This commit updates the route registration process to include
    authentication, role-based access control, and rate limiting. Changes
    include:
    
    - Adding AuthMiddleware for authenticating requests using an API key.
    - Adding RequireRole middleware to enforce role-based access control for
    admin routes.
    - Adding RateLimitMiddleware to limit the number of requests for
    registration and login endpoints.
    - Registering routes for user registration and login.
    - Securing currency routes with authentication and role-based access
    control.

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"github.com/Lutefd/challenge-bravo/internal/handler"
+	api_middleware "github.com/Lutefd/challenge-bravo/internal/middleware"
+	"github.com/Lutefd/challenge-bravo/internal/model"
 	"github.com/Lutefd/challenge-bravo/internal/service"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -10,12 +12,19 @@ import (
 func (s *Server) registerRoutes(currencyService *service.CurrencyService) {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
+	authMiddleware := api_middleware.NewAuthMiddleware(s.userRepo)
+
 	router.Get("/healthz", handler.HandlerReadiness)
 	currencyHandler := handler.NewCurrencyHandler(currencyService)
+	userHandler := handler.NewUserHandler(s.userRepo)
+	router.Route("/auth", func(r chi.Router) {
+		r.With(api_middleware.RateLimitMiddleware).Post("/register", userHandler.Register)
+		r.With(api_middleware.RateLimitMiddleware).Post("/login", userHandler.Login)
+	})
 	router.Route("/currency", func(r chi.Router) {
 		r.Get("/convert", currencyHandler.ConvertCurrency)
-		r.Post("/", currencyHandler.AddCurrency)
-		r.Delete("/{code}", currencyHandler.RemoveCurrency)
+		r.With(authMiddleware.Authenticate, api_middleware.RequireRole(model.RoleAdmin)).Post("/", currencyHandler.AddCurrency)
+		r.With(authMiddleware.Authenticate, api_middleware.RequireRole(model.RoleAdmin)).Delete("/{code}", currencyHandler.RemoveCurrency)
 	})
 	s.router = router
 }
