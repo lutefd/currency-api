6925920e2b8b489c098b78b38416d29ed0bbd46d
Luis Dourado <lfaddourado@gmail.com>
2024-08-05 12:55:58 -0300

    feat(repository): add UserRepository interface
    This commit adds the UserRepository interface to the project. The
    UserRepository interface includes the following methods:
    
    - Create: Adds a new user to the repository.
    - GetByUsername: Retrieves a user by their username.
    - GetByAPIKey: Retrieves a user by their API key.
    - Close: Closes the repository connection.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -13,3 +13,10 @@ type CurrencyRepository interface {
 	Delete(ctx context.Context, code string) error
 	Close() error
 }
+
+type UserRepository interface {
+	Create(ctx context.Context, user *model.User) error
+	GetByUsername(ctx context.Context, username string) (*model.User, error)
+	GetByAPIKey(ctx context.Context, apiKey string) (*model.User, error)
+	Close() error
+}
