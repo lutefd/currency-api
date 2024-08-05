87de538f0152fb73821c515d1a118174b6df2341
Luis Dourado <lfaddourado@gmail.com>
2024-08-05 09:58:40 -0300

    feat(cache): create cache package and define its interface
    This commit defines the cache interface with the following methods:
    
    - Get: Retrieves a value from the cache by key.
    - Set: Stores a value in the cache with a specified expiration time.
    - Delete: Removes a value from the cache by key.

---
+++ internal/cache/redis_cache.go
@@ -0,0 +1,12 @@
+package cache
+
+import (
+	"context"
+	"time"
+)
+
+type Cache interface {
+	Get(ctx context.Context, key string) (float64, error)
+	Set(ctx context.Context, key string, value float64, expiration time.Duration) error
+	Delete(ctx context.Context, key string) error
+}
