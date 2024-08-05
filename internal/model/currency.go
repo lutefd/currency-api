b784022c9ee4e0bf3c30b1443a73a017d1218f7b
Luis Dourado <lfaddourado@gmail.com>
2024-08-05 09:22:42 -0300

    feat(model): create currency and exchange rates model
    This commit adds the data models for the currency and exchange rate
    entities

--- internal/model/currency.go
+++ internal/model/currency.go
@@ -1,1 +1,19 @@
 package model
+
+import (
+	"time"
+)
+
+type Currency struct {
+	Code      string    `json:"code"`
+	Rate      float64   `json:"rate"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type ExchangeRates struct {
+	Disclaimer string             `json:"disclaimer"`
+	License    string             `json:"license"`
+	Timestamp  int64              `json:"timestamp"`
+	Base       string             `json:"base"`
+	Rates      map[string]float64 `json:"rates"`
+}
