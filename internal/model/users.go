4304b3a02287e37d7ab8d903c9869134876f68d6
Luis Dourado <lfaddourado@gmail.com>
2024-08-05 13:30:10 -0300

    refactor(model): remove unecessary fields from User model
    This commit removes the following fields from the User struct that is
    used to shape the JSON response for the login:
    
    - CreatedAt
    - UpdatedAt

--- internal/model/users.go
+++ internal/model/users.go
@@ -24,12 +24,10 @@ type UserDB struct {
 }
 
 type User struct {
-	ID        uuid.UUID `json:"id"`
-	Username  string    `json:"username"`
-	Role      Role      `json:"role"`
-	APIKey    string    `json:"api_key"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Role     Role      `json:"role"`
+	APIKey   string    `json:"api_key"`
 }
 
 func (u *UserDB) ToUser() User {
