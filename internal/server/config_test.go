ce23e8a0666b9be63e57882ae09f0041fc81163e
Luis Dourado <lfaddourado@gmail.com>
2024-08-07 17:07:31 -0300

    test(config): add tests for LoadConfig function
    This commit adds unit tests for the `LoadConfig` function to ensure
    proper configuration loading and error handling.
    
    Changes:
    
    - Created `config_test.go` with multiple test cases.
    - Added tests for valid configuration loading.
    - Added tests for missing environment variables.
    - Added tests for invalid `SERVER_PORT` values.
    - Added tests for partial configuration scenarios

---
+++ internal/server/config_test.go
@@ -0,0 +1,76 @@
+package server_test
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/Lutefd/challenge-bravo/internal/server"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadConfig(t *testing.T) {
+	originalEnv := os.Environ()
+	defer func() {
+		os.Clearenv()
+		for _, pair := range originalEnv {
+			parts := strings.SplitN(pair, "=", 2)
+			os.Setenv(parts[0], parts[1])
+		}
+	}()
+
+	setEnv := func(key, value string) {
+		os.Setenv(key, value)
+	}
+
+	t.Run("Valid configuration", func(t *testing.T) {
+		setEnv("REDIS_PASSWORD", "password")
+		setEnv("REDIS_ADDR", "localhost:6379")
+		setEnv("POSTGRES_CONN", "postgres://user:pass@localhost:5432/db")
+		setEnv("API_KEY", "my-api-key")
+		setEnv("SERVER_PORT", "8080")
+
+		config, err := server.LoadConfig()
+
+		assert.NoError(t, err)
+		assert.Equal(t, "password", config.RedisPass)
+		assert.Equal(t, "localhost:6379", config.RedisAddr)
+		assert.Equal(t, "postgres://user:pass@localhost:5432/db", config.PostgresConn)
+		assert.Equal(t, "my-api-key", config.APIKey)
+		assert.Equal(t, uint16(8080), config.ServerPort)
+	})
+
+	t.Run("Missing environment variables", func(t *testing.T) {
+		os.Clearenv()
+
+		_, err := server.LoadConfig()
+
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "configuration errors occurred")
+	})
+
+	t.Run("Invalid SERVER_PORT", func(t *testing.T) {
+		setEnv("REDIS_PASSWORD", "password")
+		setEnv("REDIS_ADDR", "localhost:6379")
+		setEnv("POSTGRES_CONN", "postgres://user:pass@localhost:5432/db")
+		setEnv("API_KEY", "my-api-key")
+		setEnv("SERVER_PORT", "invalid-port")
+
+		_, err := server.LoadConfig()
+
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "configuration errors occurred")
+	})
+
+	t.Run("Partial configuration", func(t *testing.T) {
+		os.Clearenv()
+		setEnv("REDIS_PASSWORD", "password")
+		setEnv("REDIS_ADDR", "localhost:6379")
+
+		_, err := server.LoadConfig()
+
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "configuration errors occurred")
+
+	})
+}
